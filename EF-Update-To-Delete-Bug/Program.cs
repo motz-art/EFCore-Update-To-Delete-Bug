35804eb94e6aa4807f63066ec263665408de3056
Oleg Oshkoderov <oleg.oshkoderov@tsceu.com>
2018-11-07 15:34:23 +0100

    Add project files.

---
+++ EF-Update-To-Delete-Bug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EF_Update_To_Delete_Bug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
